a29007ba09185e77111395d08149e4e13d35ec02
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-07-24 21:56:53 -0700

    feat(yc-syntax): add ast types

--- yc-syntax/src/ast/mod.rs
+++ yc-syntax/src/ast/mod.rs
@@ -103,3 +103,244 @@ impl PartialOrd for Position {
         Some(self.cmp(other))
     }
 }
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Ast {
+    pub stmts: Vec<Stmt>,
+    pub comments: Vec<Comment>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Stmt {
+    Text(Text),
+
+    Block(BlockStmt),
+    Define(DefineStmt),
+    ConditionalBranch(ConditionalBranchStmt),
+    Range(RangeStmt),
+    While(WhileStmt),
+    LoopControl(LoopControlStmt),
+    Try(TryStmt),
+    Return(ReturnStmt),
+    Expr(ExprStmt),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Text {
+    pub span: Span,
+    pub content: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct BlockStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub context: Option<Expr>,
+    pub body: Vec<Stmt>,
+    pub end: EndStmt,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DefineStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub name: StringLit,
+    pub body: Vec<Stmt>,
+    pub end: EndStmt,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ConditionalBranchStmt {
+    pub kind: BranchKind,
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub cond: Expr,
+    pub body: Vec<Stmt>,
+    pub else_branches: Vec<ElseBranch>,
+    pub end: EndStmt,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum BranchKind {
+    If,
+    With,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ElseBranch {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub cond: Option<Expr>,
+    pub body: Vec<Stmt>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct RangeStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub element_binding: Option<VarName>,
+    pub index_binding: Option<VarName>,
+    pub expr: Expr,
+    pub body: Vec<Stmt>,
+    pub end: EndStmt,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct WhileStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub cond: Expr,
+    pub body: Vec<Stmt>,
+    pub end: EndStmt,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct LoopControlStmt {
+    pub span: Span,
+    pub kind: LoopControlKind,
+    pub trim_markers: TrimMarkers,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum LoopControlKind {
+    Break,
+    Continue,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct TryStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub try_body: Vec<Stmt>,
+    pub catch: CatchStmt,
+    pub catch_body: Vec<Stmt>,
+    pub end: EndStmt,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct CatchStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct EndStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ReturnStmt {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub expr: Expr,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ExprStmt {
+    pub span: Span,
+    pub expr: Expr,
+    pub trim_markers: TrimMarkers,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct TrimMarkers {
+    pub left: bool,
+    pub right: bool,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Comment {
+    pub span: Span,
+    pub trim_markers: TrimMarkers,
+    pub content: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expr {
+    BoolLit(BoolLit),
+    CharLit(CharLit),
+    FloatLit(FloatLit),
+    IntLit(IntLit),
+    NilLit(NilLit),
+    StringLit(StringLit),
+
+    FnCall(FnCallExpr),
+    Pipeline(ExprPipeline),
+    VarAssign(VarAssignExpr),
+    VarDef(VarDefExpr),
+    VarRef(VarRefExpr),
+}
+
+macro_rules! define_lit {
+    ($t:ty, $name:ident) => {
+        #[derive(Clone, Debug, PartialEq)]
+        pub struct $name {
+            pub span: Span,
+            pub val: $t,
+        }
+    };
+}
+
+define_lit!(bool, BoolLit);
+define_lit!(char, CharLit);
+define_lit!(f64, FloatLit);
+define_lit!(i64, IntLit);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct StringLit {
+    pub span: Span,
+    pub kind: StringLitKind,
+    pub val: String,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum StringLitKind {
+    Raw,
+    Interpreted,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct NilLit {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct VarAssignExpr {
+    pub span: Span,
+    pub name: VarName,
+    pub expr: Box<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct VarDefExpr {
+    pub span: Span,
+    pub name: VarName,
+    pub expr: Box<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct VarRefExpr {
+    pub span: Span,
+    pub name: VarName,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct VarName {
+    pub span: Span,
+    pub val: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+
+pub struct FnCallExpr {
+    pub span: Span,
+    pub name: String,
+    pub args: Vec<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ExprPipeline {
+    pub init: Box<Expr>,
+    pub fn_calls: Vec<FnCallExpr>,
+}
