d12a602e13189b16b850a93581679de9073f9cd3
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-08-22 13:14:32 -0700

    feat(yc-parser): add TokenPattern and related helpers

--- yc-parser/src/parse/mod.rs
+++ yc-parser/src/parse/mod.rs
@@ -1,15 +1,15 @@
 mod token_cursor;
 
 use yc_ast::ast;
-use yc_ast::location::BytePos;
 use yc_ast::node_id::NodeId;
+use yc_ast::token::{Token, TokenKind};
 use yc_diagnostics::Diagnostic;
 
 use crate::lex::Lexer;
 use crate::parse::token_cursor::TokenCursor;
 
 pub struct Parser<'src> {
-    tokens: TokenCursor<'src>,
+    cursor: TokenCursor<'src>,
     diagnostics: Vec<Diagnostic>,
     node_id: NodeId,
 }
@@ -17,7 +17,7 @@ pub struct Parser<'src> {
 impl<'src> Parser<'src> {
     pub fn new(lexer: Lexer<'src>) -> Self {
         Self {
-            tokens: TokenCursor::new(lexer),
+            cursor: TokenCursor::new(lexer),
             diagnostics: vec![],
             node_id: NodeId::from_usize(0),
         }
@@ -27,6 +27,40 @@ impl<'src> Parser<'src> {
         todo!()
     }
 
+    fn at(&mut self, pat: impl TokenPattern) -> bool {
+        pat.matches(self.cursor.first())
+    }
+
+    fn at_ignore_spaces(&mut self, pat: impl TokenPattern) -> bool {
+        pat.matches(self.cursor.first_non_space())
+    }
+
+    fn nth_at(&mut self, n: usize, pat: impl TokenPattern) -> bool {
+        pat.matches(self.cursor.nth(n))
+    }
+
+    fn nth_at_ignore_spaces(&mut self, n: usize, pat: impl TokenPattern) -> bool {
+        pat.matches(self.cursor.nth_non_space(n))
+    }
+
+    fn eat(&mut self, pat: impl TokenPattern) -> bool {
+        if pat.matches(self.cursor.first()) {
+            self.cursor.bump();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn eat_ignore_spaces(&mut self, pat: impl TokenPattern) -> bool {
+        if pat.matches(self.cursor.first_non_space()) {
+            self.cursor.bump();
+            true
+        } else {
+            false
+        }
+    }
+
     fn add_diagnostic(&mut self, diagnostic: Diagnostic) {
         self.diagnostics.push(diagnostic);
     }
@@ -34,6 +68,22 @@ impl<'src> Parser<'src> {
     fn next_node_id(&mut self) -> NodeId {
         self.node_id += 1;
         self.node_id
+    }
+}
+
+pub(crate) trait TokenPattern {
+    fn matches(self, token: Token) -> bool;
+}
+
+impl TokenPattern for TokenKind {
+    fn matches(self, token: Token) -> bool {
+        token.kind == self
+    }
+}
+
+impl<'a, const N: usize> TokenPattern for &'a [TokenKind; N] {
+    fn matches(self, token: Token) -> bool {
+        self.contains(&token.kind)
     }
 }
 
